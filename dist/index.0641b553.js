535b3dfe6196e8a67395cdca1343b72f05dae488
sfundomhlungu <devadress21@gmail.com>
2022-11-04 10:38:25 +0200

    fini frame model

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -944,7 +944,7 @@ const projects_model = [
 ];
 const projectDetail = {
     Frame: {
-        longer_description: "Frame is actually a combination of a dataframe inspired by the Python pandas library, written in pure Typescript and Machine Learning using ML5, the sole purpose of frame is to iteratively manipulate, clean and fit Machine Learning models to tabular data without touching or seeing code, making ML accessible and quick for anyone and it's all on the client side thanks to web workers.",
+        longer_description: "Frame is actually a combination of a dataframe inspired by the Python pandas library, written in pure Typescript and Machine Learning using ML5, the sole purpose of frame is to iteratively manipulate, clean and fit Machine Learning models to tabular data without touching or seeing code, making ML accessible and quick for anyone and it's all on the client side thanks to web workers and entirely written in JavaScript, HTML and CSS.",
         project_implementation: [
             "Frame has 3 major components, the first component is responsible for creating self contained, self aware stateful HTML elements, the second is responsible for communication between elements created by component 1 and the lower level dataframe code or thread and fitting neaural networks, while the last is only responsible for user feedback, downloading files, ML models, connecting to a database and user authentication.",
             "Frame is way more than a CRUD app, for example each session can have multiple elements of the same type e.g 3 scatter charts, the afore mentioned components make sure these elements are independent, manage their own state, don't intermingle and confuse each other even thou they are similar."
@@ -952,21 +952,27 @@ const projectDetail = {
         stack_explanation: {
             stack: [
                 {
-                    icon: "./svg/next-js.svg",
-                    name: "next.js"
+                    icon: "./man-technologist-medium-skin-tone.09a6d491.svg",
+                    name: "bokke.js"
                 },
                 {
-                    name: "Ionic",
-                    icon: "./svg/file-type-ionic.svg"
+                    icon: "./ml5.png",
+                    name: "ML5"
                 },
                 {
-                    icon: "./svg/typescript-icon.svg",
-                    name: "Typescript"
+                    name: "Atomicus",
+                    icon: "./man-technologist-medium-skin-tone.09a6d491svg"
+                },
+                {
+                    icon: "./svg/express-original.svg",
+                    name: "Express"
                 }
             ],
             explanations: [
-                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea ipsa inventore in, tempore iure cum a obcaecati, sint quos dolorum ipsam harum qui! Suscipit minus nobis commodi labore necessitatibus aspernatur.",
-                "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea ipsa inventore in, tempore iure cum a obcaecati, sint quos dolorum ipsam harum qui! Suscipit minus nobis commodi labore necessitatibus aspernatur."
+                "Bokke.js is at the core of Frame, functionality wise, as it is responsible for turning csv to tabular data, visualization, manipulation and data cleaning, the coolest part propably is bokke.js run's on a seperate thread isolating large data and heavy calculations from the main thread, which only handles UI updates, I built bokke.js entirely with Typescript which makes it robust and easily mantainable.",
+                "Ml5 is responsible for everything Machine Learning, from fitting models, to downloading them to be used in the browser, as ML5 is built on top of tensorflow.js",
+                "Atomicus another frontend tool I built is probably the heavy lifter of this web app in terms of frontend, which is responsible for creating self contained, self aware stateful HTML elements, and establishing communication between them using the MVC pattern, and facilitating communication also between elements and ML5, elements and Bokke.js",
+                "Express is responsible for everything server side, authentication, signup, saving models to a remote database with an account"
             ]
         },
         visuals: [
@@ -975,8 +981,7 @@ const projectDetail = {
             "./projects/frame/4.png"
         ],
         problems: [
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea ipsa inventore in, tempore iure cum a obcaecati, sint quos dolorum ipsam harum qui! Suscipit minus nobis commodi labore necessitatibus aspernatur.",
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea ipsa inventore in, tempore iure cum a obcaecati, sint quos dolorum ipsam harum qui! Suscipit minus nobis commodi labore necessitatibus aspernatur."
+            "The biggest hurdle I came across was creating dynamic elements and handling their states, while libraries and frameworks can handle that easily, I was working in pure JavaScript so I had to be creative and started researching how these frameworks achieve this internally, which led to the implementation of Atomicus, inspired by React's virtual DOM, on top of the V DOM, I added the MVC pattern naturally in Atomicus using reactive patterns, which overcame efficiently the problem of creating dynamic and stateful HTML elements."
         ],
         featured_code: [
             {
@@ -985,7 +990,8 @@ const projectDetail = {
             }
         ],
         lessons: [
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea ipsa inventore in, tempore iure cum a obcaecati, sint quos dolorum ipsam harum qui! Suscipit minus nobis commodi labore necessitatibus aspernatur."
+            "Amongst many the most valuable lesson I learned here is the power of design patterns, and how they foster efficient and iterative development, for example I had a working MVP of frame in about 11 days because of the MVC pattern and it's efficiency for iterative development.",
+            "Frame is compute intensive and heavy, by using web workers I had an opportunity to learn and experience how to bring compute intensive applications to the client side."
         ],
         lesson_vis: [],
         isWhite: true
